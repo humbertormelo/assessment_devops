7f18d64ce04cc4e3596ab9b348ab2959f624e4e3
Humberto <humberto_rmelo@icloud.com>
2022-08-17 14:59:16 +1000

    New mesage will show up after the program check user's information

--- DevOps-Assessment/Program.cs
+++ DevOps-Assessment/Program.cs
@@ -3,10 +3,10 @@ Console.WriteLine("Please, enter your age: ");
 int age = int.Parse(Console.ReadLine());
 
 if (age > 17){
-    Console.WriteLine("You can drink alchool ");
+    Console.WriteLine("You can drink alchool because you are " + age + " years old.");
 }
 else{
-    Console.WriteLine("You can not drink alchool");
+    Console.WriteLine("You can not drink alchool because you are " + age + " years old.");
 }
 
 
