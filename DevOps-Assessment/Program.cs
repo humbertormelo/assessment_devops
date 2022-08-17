3403bcc05051db4b2cff36d8be50b5197c301c31
Humberto <humberto_rmelo@icloud.com>
2022-08-17 14:54:09 +1000

    Reading the age using Convert instead int.parse

--- DevOps-Assessment/Program.cs
+++ DevOps-Assessment/Program.cs
@@ -1,6 +1,6 @@
 ﻿
 Console.WriteLine("Please, enter your age: ");
-int age = int.Parse(Console.ReadLine());
+int age = Convert.ToInt32(Console.ReadLine());
 
 if (age > 17){
     Console.WriteLine("You can drink alchool ");
