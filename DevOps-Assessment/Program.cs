1e4f6cc6bcabb783285bef6b053ebd852216bd9c
Humberto Rodrigues de Melo <humberto_rmelo@icloud.com>
2022-08-18 08:24:20 +1000

    Unit test files

--- DevOps-Assessment/Program.cs
+++ DevOps-Assessment/Program.cs
@@ -1,14 +1,28 @@
-﻿Console.WriteLine("Please, enter your name ");
-string name = Console.ReadLine();
-Console.WriteLine("Please, enter your age: ");
-int age = Convert.ToInt32(Console.ReadLine());
+﻿using System;
 
-if (age > 17){
-    Console.WriteLine("You can drink alchool because you are " + age + " years old.");
-}
-else{
-    Console.WriteLine("You can not drink alchool because you are " + age + " years old.");
-}
+class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter your name ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please, enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age > 17)
+            {
+                Console.WriteLine("You can drink alchool because you are " + age + " years old.");
+            }
+            else
+            {
+                Console.WriteLine("You can not drink alchool because you are " + age + " years old.");
+            }
+        }
+    }
+
+
+
+
 
 
 
