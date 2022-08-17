8cffefec2cf03257a56db885c514a07cacc3d72f
Humberto <humberto_rmelo@icloud.com>
2022-08-17 13:10:22 +1000

    Sending the code to the github

--- DevOps-Assessment/Program.cs
+++ DevOps-Assessment/Program.cs
@@ -2,12 +2,10 @@
 Console.WriteLine("Please, enter your age: ");
 int age = int.Parse(Console.ReadLine());
 
-if (age > 17)
-{
+if (age > 17){
     Console.WriteLine("You can drink alchool ");
 }
-else
-{
+else{
     Console.WriteLine("You can not drink alchool");
 }
 
