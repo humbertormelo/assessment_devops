7e0da699e0e33b148517dd7dfa653b0c926390af
Humberto <humberto_rmelo@icloud.com>
2022-08-17 13:01:14 +1000

    Add project files.

---
+++ DevOps-Assessment/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Please, enter your age: ");
+int age = int.Parse(Console.ReadLine());
+
+if (age > 17)
+{
+    Console.WriteLine("You can drink alchool ");
+}
+else
+{
+    Console.WriteLine("You can not drink alchool");
+}
+
+
+
+
